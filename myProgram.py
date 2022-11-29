9642d25ea5206a46bc543a21dcd0a31513edf4a7
AA123SKY <aaron2003@live.com.sg>
2022-11-29 10:38:06 +0800

    test

--- myProgram.py
+++ myProgram.py
@@ -1,1 +1,11 @@
-print("huh")
+# print("huh")
+# print("hi")
+
+import unittest
+
+class TestMyProgram(unittest.TestCase):
+    def test_EngineType(self):
+        print("Testing")
+
+if __name__ == '__main__':
+    unittest.main()
